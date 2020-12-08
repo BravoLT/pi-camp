ba2089f07733005fe3aebf20672c0e3781dee0f3
Whit Wu <whit.wu@gmail.com>
2020-12-07 22:06:17 -0500

    Write first 3 lessons

---
+++ lesson1.js
@@ -0,0 +1,12 @@
+// Lesson 1: Hello World
+
+
+// Hello World is often the first program a developer
+// writes when learning a new language.  in javescript,
+// we can write this program by displaying output to the
+// console with the console.log function, like so:
+console.log("Hello World!");
+
+
+// Challenge
+// Write a message to the console with console.log
