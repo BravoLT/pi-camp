00bba0dd3b4f48733b9d099b1fe2145e3820cc79
Thomas <lyz27@yahoo.com>
2020-12-14 20:22:52 -0500

    added lesson 6 stub

---
+++ lesson6.js
@@ -0,0 +1,13 @@
+/* Lesson 6 loops and groups of things
+Coding is all about automation of mundane tasks.
+Who wants to put away dishes when you can program something else to do it.
+If we have 5 dishes, do we say....
+Put away dish 
+Put away dish
+Put away dish
+Put away dish
+Put away dish  
+What about if we have 500 dishes to put away?  50,000?
+Put the dishes away. Is how we say it... to code it we say something else.....
+
+
